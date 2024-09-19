82c01a63e7efa838f83b10c5d656619392548b80
Andy Pak <im.andy.pak@gmail.com>
2024-09-18 21:34:51 -0400

    Fix player win conditions

--- lib/board.rb
+++ lib/board.rb
@@ -7,6 +7,6 @@ class Board
   attr_accessor :secret_code, :moves, :current_pins
 
   def check_winner
-    current_pins.all? { |pin| pin == :red }
+    current_pins.all? { |pin| pin == :red } && current_pins.length == 4
   end
 end
